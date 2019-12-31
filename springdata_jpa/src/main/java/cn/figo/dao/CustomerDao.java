9bc4ec70ed09947bf1720f8d2ac1e9bf2ddde84a
YourFigo <501385003@qq.com>
2019-12-31 15:13:55 +0800

    spring data jpa 实体类和持久化接口

---
+++ springdata_jpa/src/main/java/cn/figo/dao/CustomerDao.java
@@ -0,0 +1,21 @@
+package cn.figo.dao;
+
+import cn.figo.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ * @Author Figo
+ * @Date 2019/12/31 15:09
+ */
+/**
+ * 符合SpringDataJpa的dao层接口规范
+ *      JpaRepository<操作的实体类类型，实体类中主键属性的类型>
+ *          * 封装了基本CRUD操作
+ *      JpaSpecificationExecutor<操作的实体类类型>
+ *          * 封装了复杂查询（分页）
+ */
+public interface CustomerDao extends JpaRepository<Customer,Long>, JpaSpecificationExecutor<Customer> {
+
+}
