34aba4683d60ada2545f2fc9a70002a53405b8c9
YourFigo <501385003@qq.com>
2020-01-08 23:33:16 +0800

    spring data jpa 多对多操作

---
+++ springdata_jpa_manytomany/src/main/java/cn/figo/dao/UserDao.java
@@ -0,0 +1,8 @@
+package cn.figo.dao;
+
+/**
+ * @Author Figo
+ * @Date 2020/1/8 23:32
+ */
+public interface UserDao {
+}
---
+++ springdata_jpa_manytomany/src/main/java/cn/figo/domain/User.java
@@ -0,0 +1,8 @@
+package cn.figo.domain;
+
+/**
+ * @Author Figo
+ * @Date 2020/1/8 23:31
+ */
+public class User {
+}
