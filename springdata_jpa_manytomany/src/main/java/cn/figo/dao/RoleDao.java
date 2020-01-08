908171774743ad0c1baea404e552d7e69b37174e
YourFigo <501385003@qq.com>
2020-01-08 23:44:00 +0800

    spring data jpa 多对多操作  配置实体的多对多

--- springdata_jpa_manytomany/src/main/java/cn/figo/dao/RoleDao.java
+++ springdata_jpa_manytomany/src/main/java/cn/figo/dao/RoleDao.java
@@ -1,8 +1,12 @@
 package cn.figo.dao;
 
+import cn.figo.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
 /**
  * @Author Figo
  * @Date 2020/1/8 23:32
  */
-public interface RoleDao {
+public interface RoleDao extends JpaRepository<Role,Long>, JpaSpecificationExecutor<Role> {
 }
--- springdata_jpa_manytomany/src/main/java/cn/figo/dao/UserDao.java
+++ springdata_jpa_manytomany/src/main/java/cn/figo/dao/UserDao.java
@@ -1,8 +1,12 @@
 package cn.figo.dao;
 
+import cn.figo.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
 /**
  * @Author Figo
  * @Date 2020/1/8 23:32
  */
-public interface UserDao {
+public interface UserDao extends JpaRepository<User,Long>, JpaSpecificationExecutor<User> {
 }
